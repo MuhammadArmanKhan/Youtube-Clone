c85a3c2068657f83ad135bfcaca6349e020a513f
MuhammadArmanKhan <armankhanp766@gmail.com>
2024-02-04 14:50:08 +0500

    Importing files and MUI
    Adding MUI links in index.html
    Importing files in the index.js
    making index.css in src folder so that waqas can work on it
    App is the main structure of the app.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 // starting point of our application
-
+// structure of our entire application
 // basic functional component by running rafce using es7+ extension
 import React from 'react'
-
 const App = () => {
   return (
     <div>App</div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 // importing the App.js file
 import App from './App';
+import './index.css';
 
 // root of our application
+// fetching from the DOM (Document Object Model)
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// const root = ReactDOM.createRoot()
+root.render(<App />);
